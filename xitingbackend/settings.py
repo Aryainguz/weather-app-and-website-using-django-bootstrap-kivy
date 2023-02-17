ec611717652b1cdee575f4cfc1ccfb5220973906
Aryainguz <aryaninguz369@gmail.com>
2023-02-18 00:17:39 +0530

    vvv

--- xitingbackend/settings.py
+++ xitingbackend/settings.py
@@ -119,8 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles_build",'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static"),
 ]
